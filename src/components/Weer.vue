8c48673d5da3f1db0c1095cb98d1c07198b1f758
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2026-02-11 16:35:37 +0100

    Titel weersvoorspellingen toegevoegd

--- src/components/Weer.vue
+++ src/components/Weer.vue
@@ -69,6 +69,9 @@
     </v-col>
   </v-row>
   <v-row>
+    <v-col cols="12">
+      <h3>Weersvoorspellingen (14 dagen vooruit)</h3>
+    </v-col>
     <v-col cols="12" md="6">
       <v-card>
         <v-card-text>
